e88a25851e57439c40655ac603d8ae0b055e72e8
Giovanni <kevin.cano.pc@gmail.com>
2024-04-23 16:56:12 -0600

    ADD: INIT

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProyectoProgramacion1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
